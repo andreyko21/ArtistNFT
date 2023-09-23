e8a0164d1f822161a5d31c4f1f3a0828c12dc102
unknown <andreyko2019>
2023-09-23 15:46:16 +0300

    adaptive header

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,4 +1,23 @@
+import $ from "jquery";
 
-class Header{
-    
+export default class Header{
+    constructor(){
+        this.burgerMenuButton = $('.burger-menu-button');
+        this.burgerMenu = $('.menu');
+        this.BindEvents();
+    }
+
+    BindEvents(){
+        this.burgerMenuButton.click(()=>this.ToggleBurgerMenu())
+    }
+
+    ToggleBurgerMenu() {
+        this.burgerMenuButton.toggleClass('burger-menu-button_open');
+        if (this.burgerMenu.hasClass('menu_open')) {
+            this.burgerMenu.fadeOut(300);
+        } else {
+            this.burgerMenu.fadeIn(300);
+        }
+        this.burgerMenu.toggleClass('menu_open');
+    }
 }
