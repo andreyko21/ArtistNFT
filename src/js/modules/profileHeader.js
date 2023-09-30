89ce91f9b21cff53b16d4768fbff9fc9ebddd312
andreyko21 <andrey.zakharkiv.2001@gmail.com>
2023-09-30 17:54:25 +0300

    add chat-page

--- src/js/modules/profileHeader.js
+++ src/js/modules/profileHeader.js
@@ -1,25 +1,38 @@
 import $ from 'jquery';
-import Dropdown from './dropdown';
 
 export default class Header {
   constructor() {
     this.burgerMenuButton = $('.burger-menu-button');
-    this.burgerMenu = $('.menu');
+    this.burgerMenu = $('.profile-sidebar');
+    this.siliderBackground = $('.silider-background');
     this.BindEvents();
     this.dropDown = new Dropdown();
   }
 
   BindEvents() {
     this.burgerMenuButton.click(() => this.ToggleBurgerMenu());
+    this.siliderBackground.click(() => this.ToggleBurgerMenu());
+  }
+
+  CheckScreenSize() {
+    const windowWidth = $(window).width();
+    if (windowWidth <= 1150) {
+    } else {
+      this.dropdown.insertAfter('.menu');
+    }
   }
 
   ToggleBurgerMenu() {
     this.burgerMenuButton.toggleClass('burger-menu-button_open');
+
     if (this.burgerMenu.hasClass('menu_open')) {
       this.burgerMenu.fadeOut(300);
+      this.siliderBackground.fadeOut(300);
     } else {
       this.burgerMenu.fadeIn(300);
+      this.siliderBackground.fadeIn(300);
     }
+    this.siliderBackground.toggleClass('silider-background_open');
     this.burgerMenu.toggleClass('menu_open');
   }
 }
---
+++ src/js/profile.js
@@ -0,0 +1,2 @@
+import Header from './modules/profileHeader';
+this.header = new Header();
