9038cb7c9b5f31b8e27b2de17ace6dd2a58d04d8
ArtemKliuiev <117043590+ArtemKliuiev@users.noreply.github.com>
2023-10-10 14:45:37 +0300

    arts in progress

--- src/js/arts-one.js
+++ src/js/arts-one.js
@@ -1,47 +1,128 @@
 import $ from 'jquery';
 import OneItem from './modules/one-item';
-import firebase from './modules/firebase';
-import { collection, doc, addDoc, getDocs, getDoc } from 'firebase/firestore';
 import Header from './modules/header';
+// import firebase from './modules/firebase';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { initializeApp } from 'firebase/app';
+import { collection, getDocs, getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCQdQY3D9G60qB3PCBhtCz-W7YynZJQzSU',
+  authDomain: 'artistnft.firebaseapp.com',
+  projectId: 'artistnft',
+  storageBucket: 'artistnft.appspot.com',
+  messagingSenderId: '1090475560828',
+  appId: '1:1090475560828:web:3ff50b4eb39bbcacd45ef9',
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+
+const querySnapshot = await getDocs(collection(db, "arts"));
+querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+  console.log(doc.id, " => ", doc.data());
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class ArtsOne {
-    constructor() {
-      this.header = new Header();
-      this.db = firebase.getFirestore();
-      this.usersCollection = collection(this.db, 'arts');
-      this.prolax();
-    }
-    init() {
-        console.log(this.usersCollection)
-    }
-    prolax(){
-      $(document).ready(function () {
-        $('.arts').mousemove(function (e) {
-          var containerWidth = $(this).width();
-          var containerHeight = $(this).height();
-          var mouseX = e.pageX - $(this).offset().left;
-          var mouseY = e.pageY - $(this).offset().top;
-          var offsetX = 0.5 - mouseX / containerWidth;
-          var offsetY = 0.5 - mouseY / containerHeight;
-      
-          $('.parallax').css({
-            transform:
-              'translate(-50%,-50%) translate(' +
-              offsetX * 40 +
-              'px,' +
-              offsetY * 40 +
-              'px)',
-          });
+  constructor() {
+    this.header = new Header();
+    this.prolax();
+  }
+  init() {
+      console.log(this.usersCollection)
+  }
+  prolax(){
+    $(document).ready(function () {
+      $('.arts').mousemove(function (e) {
+        var containerWidth = $(this).width();
+        var containerHeight = $(this).height();
+        var mouseX = e.pageX - $(this).offset().left;
+        var mouseY = e.pageY - $(this).offset().top;
+        var offsetX = 0.5 - mouseX / containerWidth;
+        var offsetY = 0.5 - mouseY / containerHeight;
+    
+        $('.parallax').css({
+          transform:
+            'translate(-50%,-50%) translate(' +
+            offsetX * 40 +
+            'px,' +
+            offsetY * 40 +
+            'px)',
         });
-    });
-  }};
-  
+      });
+  });
+}};
+
 const artsOne = new  ArtsOne();
-
-
-
-
 
 // Временный вариант 
 const allItems = document.querySelectorAll('.one-card');
