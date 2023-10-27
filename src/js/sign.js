3167426a1f1fdc61bb368b92f681a76edf5942c6
andreyko21 <andrey.zakharkiv.2001@gmail.com>
2023-10-27 15:59:54 +0300

    add parallax func

--- src/js/sign.js
+++ src/js/sign.js
@@ -7,6 +7,7 @@ import {
   signInWithEmailAndPassword,
 } from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
+import Parallax from './modules/parallax';
 
 class AuthPage {
   constructor() {
@@ -208,43 +209,4 @@ class AuthPage {
 
 new AuthPage();
 
-$(document).ready(function () {
-  $('.wrapper').mousemove(function (e) {
-    var containerWidth = $(this).width();
-    var containerHeight = $(this).height();
-    var mouseX = e.pageX - $(this).offset().left;
-    var mouseY = e.pageY - $(this).offset().top;
-    var offsetX = 0.5 + mouseX / containerWidth;
-    var offsetY = 0.5 + mouseY / containerHeight;
-
-    $('.parallax').each(function (index) {
-      var offsetX = 0.5 + mouseX / containerWidth;
-      var offsetY = 0.5 + mouseY / containerHeight;
-      var rotationAngle = Math.floor(Math.random());
-
-      if (index % 2 === 0) {
-        $(this).css({
-          transform:
-            'translate(-50%,-50%) translate(' +
-            offsetX * 40 +
-            'px,' +
-            offsetY * 40 +
-            'px) rotate(' +
-            rotationAngle +
-            'deg)',
-        });
-      } else {
-        $(this).css({
-          transform:
-            'translate(-50%,-50%) translate(' +
-            offsetX * -40 +
-            'px,' +
-            offsetY * -40 +
-            'px) rotate(' +
-            rotationAngle +
-            'deg)',
-        });
-      }
-    });
-  });
-});
+Parallax();
