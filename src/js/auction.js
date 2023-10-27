2880ff381bc72665a58cf542deafbbb634d781dc
ArtemKliuiev <117043590+ArtemKliuiev@users.noreply.github.com>
2023-10-27 16:21:44 +0300

    add auction logic

--- src/js/auction.js
+++ src/js/auction.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import Header from './modules/header';
-import { collection, getDocs} from "firebase/firestore";
+import { collection, getDocs, updateDoc, setDoc, doc} from "firebase/firestore";
 import firebase from './modules/firebase';
 
 
@@ -9,7 +9,9 @@ class Auction {
         this.header = new Header();
         this.db = firebase.getFirestore();
         this.firebase();
-        this.hiddenBtn();
+        this.auth = firebase.getAuth();
+        this.inputPrice = $('.input-block__input');
+        this.inpError = $('.auction__error-place');
         this.urlParams = new URLSearchParams(window.location.search);
         this.id = this.urlParams.get("id");
         if(this.id === null){
@@ -23,8 +25,44 @@ class Auction {
                 this.inputInfo(doc.data());
                 this.timer(doc.data().liveData);
                 this.liveData(doc.data().liveData);
+                this.progressPrice(doc.data());
+                this.dataPrice(doc.data());
+                this.priceBtn(doc.data());
+                this.hiddenBtn();
             }
         });
+    }
+    async changePrice(price, data){
+        if(this.auth.currentUser){
+            await setDoc(doc(this.db, "auction", this.id), {
+                price: price,
+                email: this.auth.currentUser.email,
+                user: this.auth.currentUser.uid,
+            });
+            await updateDoc(doc(this.db, "arts", this.id), {
+                price: price,
+            });
+            alert('Ставку змінено');
+            this.updatePrice(price, data);
+        }else{
+            alert('Для того щоб зробити ставку, потрібно авторизуватися')
+        }
+    }
+    priceBtn(data){
+        $('.auction__input-btn').on('click', () => {
+            const minBid = parseFloat(data.price) + 5;
+            const inpInfo = parseFloat(this.inputPrice.val());
+            if(minBid > inpInfo){
+                this.inpError.html('increase your bet');
+            }else if(this.inputPrice.val().length == 0){
+                this.inpError.html('input price');
+            }else if(isNaN(inpInfo)){
+                this.inpError.html('input only number');
+            }else{
+                this.inpError.html('');
+                this.changePrice(inpInfo, data);
+            }
+        })
     }
     timer(data){
         const countdownDate = new Date(data).getTime();
@@ -64,56 +102,52 @@ class Auction {
         $('#live-hours').html(minutes);
         $('#live-minutes').html(hours);
     }
+    progressPrice(data){
+        const thisPrice = parseFloat(data.price);
+        const minPrice = parseFloat(data.minPrice);
+        $(".auction__progress-status").css("width", `${(thisPrice / (minPrice / 100) )}%`);
+    }
+    updatePrice(price, data){
+        $('.auction__price').html(parseFloat(price).toLocaleString('ru-RU'));
+        const minPrice = parseFloat(data.minPrice);
+        const newWidth = (price / (minPrice / 100)) + "%";
+        $(".auction__progress-status").animate({ width: newWidth }, 1000); 
+        this.inputPrice.val('');
+        $('#input-place').attr('placeholder', '$' +  (price + 5).toLocaleString('ru-RU'));
+    }
+    dataPrice(data){
+        const startData = new Date(data.startData).getTime();
+        const liveData = new Date(data.liveData).getTime();
+        const thisDate = new Date().getTime();
+        const progress = ((thisDate - startData) / ((liveData - startData) / 100));
+        $(".auction__progress-procent").css("width", `${progress}%`);
+    }
     inputInfo(data){
-        console.log(data)
+        // const priceInfo = '$' +  parseFloat(data.price).toLocaleString('en-US');
+        // const startPrice = parseFloat(data.price).toLocaleString('en-US');
         $('.auction__name-title').html(data.title);
         $('.auction__size').html(data.details[1]);
-        $('.auction__start-price').html(data.startPrice);
+        $('.auction__start-price').html(parseFloat(data.minPrice).toLocaleString('en-US'));
         $('.auction__inform-column_next-inform').html(data.details[2]);
-        $('.auction__price').html(data.price);
-        $('#input-place').attr('placeholder', '$' + data.price );
+        $('.auction__price').html(parseFloat(data.price).toLocaleString('ru-RU'));
+        $('#input-place').attr('placeholder', '$' +  (parseFloat(data.price) + 5).toLocaleString('ru-RU'));
         $('.auction__text-block-paragraph_one').html(data.text[0]);
         $('.auction__text-block-paragraph_two').html(data.text[1]);
         $(".auction__img source").attr("srcset", data.imageWebP);
         $(".auction__img img").attr("src", data.imagePng);
     }
     hiddenBtn(){
+        const paragraphs = $('.auction__text-block-paragraphs');
+        const height = paragraphs.height();
+        paragraphs.height(height)
         $('.auction__text-block-btn').on("click", function() {
-            $('.auction__text-block').toggleClass('auction__text-block_hidden')
+            $('.auction__text-block').toggleClass('auction__text-block_hidden');
+            if ($('.auction__text-block').hasClass('auction__text-block_hidden')) {
+                paragraphs.height('50px')
+            }else{
+                paragraphs.height(height);
+            }
         });
-        const height = $('.auction__text-block-paragraphs').height()
-        var newKeyframesRule = `
-        @keyframes animationOne {
-            0% {
-                height: ${height}px;
-            }
-            10% {
-                height: ${height + 12}px;
-            }
-            90% {
-                height: 40px;
-            }
-            100% {
-                height: 50px;
-            }
-        }
-        @keyframes animationTwo {
-            0% {
-                height: 50px;
-            }
-            20% {
-                height: 38px;
-            }
-            90% {
-                height: ${height + 10}px;
-            }
-            100% {
-                height: ${height}px;
-            }
-        }
-        `;
-        const styleSheet = $('<style>').text(newKeyframesRule);
-        $('head').append(styleSheet);
     }
 };
 const auction = new Auction();
