87a35e2700c4919ef632b8c67a71eee65a6c7740
andreyko21 <andrey.zakharkiv.2001@gmail.com>
2023-11-02 17:57:04 +0200

    fix

--- src/js/forum.js
+++ src/js/forum.js
@@ -2,6 +2,14 @@ import Swiper from 'swiper';
 import $ from 'jquery';
 import Header from './modules/header';
 
+class Forum {
+  constructor() {
+    this.header = new Header();
+  }
+}
+
+const forum = new Forum();
+
 new Swiper('.forum-banner__swiper-container', {
   slidesPerView: 1.18,
   spaceBetween: 14,
